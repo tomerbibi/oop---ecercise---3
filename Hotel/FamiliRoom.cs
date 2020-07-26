15385e6cb1a84a1e9bbf0100b24d244096b957df
tomerbibi <66681821+tomerbibi@users.noreply.github.com>
2020-07-26 20:09:57 +0300

    Add files via upload

---
+++ Hotel/FamiliRoom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class FamiliRoom : Room
+    {
+        public FamiliRoom(int roomNumber, float price) : base(roomNumber, price)
+        {
+            SetPrice();
+            SetFreaturs();
+        }
+
+        protected override void SetFreaturs()
+        {
+           _featues = "four beds, bathroom, sink.";
+        }
+
+        protected override void SetPrice()
+        {
+            _price = 100;
+        }
+    }
+}
---
+++ oop - exercise - 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using Hotel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop___exercise___3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Suite royal = new Suite(1, 50);
+            Guest jack = new Guest(337859034, "jack");
+            Reception.CheckIn(jack);
+            Reception.CheckOut(royal);
+        }
+    }
+}
